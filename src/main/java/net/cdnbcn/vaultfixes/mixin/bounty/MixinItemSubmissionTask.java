09e5f8b10b81d7753f7406b677ce539dbbce3adc
Bailey Drahoss <Lillith.Drahoss@outlook.com>
2023-11-01 06:20:09 +0100

    fix ItemSubmission not pulling next available stack

--- src/main/java/net/cdnbcn/vaultfixes/mixin/bounty/MixinItemSubmissionTask.java
+++ src/main/java/net/cdnbcn/vaultfixes/mixin/bounty/MixinItemSubmissionTask.java
@@ -7,6 +7,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,20 +29,37 @@ public abstract class MixinItemSubmissionTask extends MixinTask {
         if (var2 instanceof ServerPlayer player) {
             TaskHelpers.INSTANCE.processEvent(player, TaskRegistry.ITEM_SUBMISSION, event, task -> {
                 // doIncrement
+                Inventory pinv = player.getInventory();
                 event.setCanceled(true);
                 ItemStack stack = event.getItemStack();
+                ItemStack match = stack.copy();
                 int amount = stack.getCount();
                 double remainder = Math.max(task.getAmountObtained() + (double)amount - task.getProperties().getAmount(), 0.0);
                 task.increment((double)amount - remainder);
                 stack.setCount((int)remainder);
-                event.getPlayer().getLevel().playSound(
+                player.getLevel().playSound(
                         null,
                         event.getPos(),
                         SoundEvents.ITEM_PICKUP,
                         SoundSource.PLAYERS,
                         0.7F,
-                        ((event.getPlayer().getRandom().nextFloat() - event.getPlayer().getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F
+                        ((player.getRandom().nextFloat() - player.getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F
                 );
+
+                if (remainder == 0.0) { // no more items in current stack
+                    int slot = pinv.findSlotMatchingItem(match);
+                    if (slot < 0) { // no more matching items
+                        return;
+                    }
+
+                    ItemStack newStack = pinv.getItem(slot).copy();
+                    if (newStack.isEmpty()) { // is 0 stack
+                        return;
+                    }
+
+                    pinv.setItem(slot, ItemStack.EMPTY);
+                    pinv.setItem(pinv.selected, newStack);
+                }
             });
         }
     }
