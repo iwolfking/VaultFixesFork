998b5f8706a308593117261e73193beb12dbe092
Bailey Drahoss <Lillith.Drahoss@outlook.com>
2023-11-18 13:24:27 +0100

    Catafrag restrictions comment.

--- src/main/java/net/cdnbcn/vaultfixes/mixin/CrystalDataMixin.java
+++ src/main/java/net/cdnbcn/vaultfixes/mixin/CrystalDataMixin.java
@@ -1,6 +1,7 @@
 package net.cdnbcn.vaultfixes.mixin;
 
 import iskallia.vault.item.crystal.CrystalData;
+import iskallia.vault.item.crystal.modifiers.CrystalModifiers;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -12,6 +13,11 @@ public class CrystalDataMixin {
      */
     @Overwrite(remap = false)
     public boolean canGenerateCatalystFragments() {
+        //note: it is possible to disable cata-frag on Vautls with wooden modifier (even after x number of wooden modifier)
+        // see <iskallia.vault.item.crystal.modifiers.CrystalModifiers.getList>
+        // see <iskallia.vault.core.vault.modifier.modifier.VaultLootableWeightModifier>
+        // see <iskallia.vault.block.PlaceholderBlock$Type>
+
         return true;
     }
 }
